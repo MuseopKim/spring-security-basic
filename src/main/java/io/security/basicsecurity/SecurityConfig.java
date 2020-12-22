fb166687d485688ccaee842522399eb90b83d556
ragdoll <museopkim0214@gmail.com>
2020-12-22 22:21:32 +0900

    Practice authenticated configuration methods

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -2,6 +2,7 @@ package io.security.basicsecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,47 +24,24 @@ import java.io.IOException;
 @EnableWebSecurity   // WebSecurityConfiguration, SpringWebMvcImportSelector, OAuth2ImportSelector 클래스를 import 해준다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    UserDetailsService userDetailsService;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest().authenticated();
-       http
+        http
                 .formLogin();
 
-       http
-               .logout()
-               .logoutUrl("/logout")
-               .logoutSuccessUrl("/login")
-               .addLogoutHandler(new LogoutHandler() {
-                   @Override
-                   public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-                       HttpSession session = request.getSession();
-                       session.invalidate();
-                   }
-               })
-               .logoutSuccessHandler(new LogoutSuccessHandler() {
-                   @Override
-                   public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                       response.sendRedirect("/login");
-                   }
-               })
-               .deleteCookies("remember-me")
-        .and()
-                .rememberMe()
-                .rememberMeParameter("remember")
-                .tokenValiditySeconds(3600)
-                .userDetailsService(userDetailsService);
-        http
-                .sessionManagement()
-                .maximumSessions(1)
-                .maxSessionsPreventsLogin(true)
-       .and()
-                .sessionFixation()
-                .changeSessionId()
-       ;
+
     }
 }
