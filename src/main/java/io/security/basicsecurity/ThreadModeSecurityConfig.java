fe2aee5a6700b50aa9562ea4340a95dc77aaff80
ragdoll <museopkim0214@gmail.com>
2021-01-04 11:57:18 +0900

    Understanding SecurityContext, SecurityContextHolder, Thread local

---
+++ src/main/java/io/security/basicsecurity/ThreadModeSecurityConfig.java
@@ -0,0 +1,23 @@
+package io.security.basicsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Configuration
+@EnableWebSecurity
+public class ThreadModeSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+        http
+                .formLogin()
+                ;
+
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    }
+}
